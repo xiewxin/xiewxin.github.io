0fbe3ba27dfa18ed6de59752a80649b4def6a350
xiewxin <xiewxin@addcn.com>
2020-06-07 01:39:19 +0800

    添加測試文件

---
+++ docs/laravel/app/Http/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * 測試
+     */
+    public function test()
+    {
+        dump(1);
+    }
+
+}
