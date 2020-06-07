f04f7b792b6bea2f37b2dfa8c4a263b47e8dc251
xiewxin <xiewxin@addcn.com>
2020-06-07 16:09:58 +0800

    輔助函數 arr

--- docs/laravel/routes/web.php
+++ docs/laravel/routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'home', 'namespace' => 'Home'], function () {
+    Route::group(['prefix' => '/auxiliary', 'namespace' => 'Auxiliary', 'as' => 'home.auxiliary.'], function () {
+        Route::any('/arr', 'ArrController@index');
+    });
 });
-
-Route::any('/index/test', 'IndexController@test');
